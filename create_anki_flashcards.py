e0b607db679f51a1a95c90067ca5e47a3089efc0
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-12-19 23:33:41 -0500

    Update API_KEY as constant, add logging, seperate methods, add error handling

--- create_anki_flashcards.py
+++ create_anki_flashcards.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -8,24 +9,29 @@ class FlashcardGenerator:
     def __init__(self, api_key):
         self.client = OpenAI(api_key=api_key)
 
-    def create_flashcards(self, input_file="flashcards_input.txt", output_file="flashcards_output.txt"):
+    def user_notes(self, input_file="flashcards_input.txt"):
         with open(input_file, "r") as flashcard_input:
-            user_message = (
+            user_notes = (
                 f"Can you make flashcards with questions and answers in a CSV file format. "
                 f"Please use these notes to create the flashcards {flashcard_input.read()} "
                 "Include nothing else but the questions and answers in your reply."
             )
+        return user_notes
 
+    def create_flashcards(self, user_message, output_file="flashcards_output.txt"):
         messages = [
             {"role": "system", "content": "You are a helpful assistant that creates flashcards with questions and answers."},
             {"role": "user", "content": user_message},
         ]
 
-        response = self.client.chat.completions.create(
-            model="gpt-3.5-turbo", messages=messages, temperature=0.7, max_tokens=2000
-        )
-
-        flashcards = response.choices[0].message.content
+        try:
+            response = self.client.chat.completions.create(
+                model="gpt-3.5-turbo", messages=messages, temperature=0.7, max_tokens=2000
+            )
+            flashcards = response.choices[0].message.content
+        except Exception as e:
+            logging.error(f"Error while interacting with OpenAI: {str(e)}")
+            return
 
         with open(output_file, "w") as f:
             replacements = {
@@ -39,13 +45,15 @@ class FlashcardGenerator:
 
             f.write(flashcards)
 
-        print("New flashcards created!")
+        logging.info("New flashcards created!")
 
 
 if __name__ == "__main__":
-    api_key = os.environ.get("OPENAI_KEY")
-    if not api_key:
+    API_KEY = os.environ.get("OPENAI_API_KEY")
+    if not API_KEY:
         raise ValueError("API key not found. Make sure to set OPENAI_API_KEY in your environment variables.")
 
-    generator = FlashcardGenerator(api_key)
-    generator.create_flashcards()
+    logging.basicConfig(level=logging.INFO)  # Adjust the logging level as needed
+    generator = FlashcardGenerator(API_KEY)
+    user_notes = generator.user_notes()
+    generator.create_flashcards(user_notes)
